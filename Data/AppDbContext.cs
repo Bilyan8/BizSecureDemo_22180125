f55f10a6da6290635d93d02c78b0f5f89265fb1c
Bilyan Garbev <bgarbev_22180125@unwe.bg>
2026-02-22 10:45:20 +0200

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BizSecureDemo_22180125.Models;
+namespace BizSecureDemo_22180125.Data;
+public class AppDbContext : DbContext
+{
+    public DbSet<AppUser> Users => Set<AppUser>();
+    public DbSet<Order> Orders => Set<Order>();
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BizSecureDemo_22180125.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
