dd482223be9d46992f22f80deaa1a9fc888e1223
Bilyan Garbev <bgarbev_22180125@unwe.bg>
2026-04-01 17:54:47 +0300

    Adding Nonce to prevent Replay Attack

---
+++ ViewModels/ReplayDemoVm.cs
@@ -0,0 +1,12 @@
+﻿namespace BizSecureDemo_22180125.ViewModels
+{
+    public class ReplayDemoVm
+    {
+        public decimal Balance { get; set; }
+        public string? Message { get; set; }
+        public int UserId { get; set; }
+        public decimal Amount { get; set; } = 100;
+        public string Token { get; set; } = "SECRET123";
+        public string Nonce { get; set; } = string.Empty;
+    }
+}
